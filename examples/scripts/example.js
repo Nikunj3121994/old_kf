95eaa30ac2e951811f735a79e67547b73c07da63
Mient - jan Stelling <mient-jan@mediamonks.com>
2016-01-22 13:54:40 +0100

    removing all examples, code is deprecated

--- examples/scripts/example.js
+++
@@ -1,6 +0,0 @@
-define(["require", "exports", '../../src/easelts/display/Stage', '../../src/easelts/display/Debug'], function (require, exports, Stage_1, Debug_1) {
-    var holder = document.getElementById('holder');
-    var stage = new Stage_1.default(holder, {});
-    stage.start();
-    stage.addChild(new Debug_1.default('canvas', '100% - 300', '100% - 300', '50%', '50%', '50%', '50%'));
-});
--- examples/scripts/example3.js
+++
@@ -1,7 +0,0 @@
-define(["require", "exports", '../../src/easelts/display/Stage', '../../src/easelts/display/Debug', '../../src/easelts/display/Bitmap'], function (require, exports, Stage_1, Debug_1, Bitmap_1) {
-    var holder = document.getElementById('holder');
-    var stage = new Stage_1.default(holder, true);
-    stage.addChild(new Debug_1.default);
-    stage.addChild(new Bitmap_1.default('assets/image/ninepatch_red.png', 0, 0, '100%', 0, '100%', 0));
-    stage.start();
-});
