4b6d615694d6425fd2ae76272b7ba948ed8ee68d
Mient-jan Stelling <mientjan.stelling@gmail.com>
2016-01-25 23:54:07 +0100

    adding buffer and updating mesh class

---
+++ src/core/webgl/Mesh.js
@@ -0,0 +1,30 @@
+define(["require", "exports", "./Buffer"], function (require, exports, Buffer_1) {
+    var Mesh = (function () {
+        function Mesh(gl, vertex, index) {
+            this._gl = gl;
+            this.vertexBuffer = new Buffer_1.default(gl, new Float32Array(vertex), gl.ARRAY_BUFFER, gl.STATIC_DRAW);
+            this.indexBuffer = new Buffer_1.default(gl, new Uint16Array(index), gl.ELEMENT_ARRAY_BUFFER, gl.STATIC_DRAW);
+            this.length = index.length;
+        }
+        Mesh.createQuad = function (gl) {
+            var vertices = [
+                -1.0, 1.0, 0.0,
+                -1.0, -1.0, 0.0,
+                1.0, -1.0, 0.0,
+                1.0, 1.0, 0.0
+            ];
+            var indices = [3, 2, 1, 3, 1, 0];
+            return new Mesh(gl, vertices, indices);
+        };
+        Mesh.prototype.bind = function () {
+            this.vertexBuffer.bind();
+            this.indexBuffer.bind();
+        };
+        Mesh.prototype.unbind = function () {
+            this.vertexBuffer.unbind();
+            this.indexBuffer.unbind();
+        };
+        return Mesh;
+    })();
+    exports.Mesh = Mesh;
+});
--- src/core/webgl/Mesh.ts
+++ src/core/webgl/Mesh.ts
@@ -1,3 +1,4 @@
+import Buffer from "./Buffer";
 export class Mesh
 {
     public static createQuad(gl:WebGLRenderingContext):Mesh
@@ -10,75 +11,33 @@ export class Mesh
         ];
 
         var indices = [3, 2, 1, 3, 1, 0];
-        return new Mesh(vertices, indices);
+        return new Mesh(gl, vertices, indices);
     }
 
-    public vertex:Float32Array;
-    public index:Uint16Array;
+    protected _gl:WebGLRenderingContext;
     public length:number;
 
-    public vertexBuffer:WebGLBuffer;
-    public indexBuffer:WebGLBuffer;
+    public vertexBuffer:Buffer;
+    public indexBuffer:Buffer;
 
-    constructor(vertex:Array<number>, index:Array<number>)
+    constructor(gl:WebGLRenderingContext, vertex:Array<number>, index:Array<number>)
     {
 
-        this.vertex = new Float32Array(vertex);
-        this.index = new Uint16Array(index);
-        this.length = this.index.length;
+        this._gl = gl;
+        this.vertexBuffer = new Buffer(gl, new Float32Array(vertex), gl.ARRAY_BUFFER, gl.STATIC_DRAW);
+        this.indexBuffer = new Buffer(gl, new Uint16Array(index), gl.ELEMENT_ARRAY_BUFFER, gl.STATIC_DRAW);
+        this.length = index.length;
     }
 
-    initBuffer(gl:WebGLRenderingContext){
-        if(!this.vertexBuffer)
-        {
-            // Create an empty buffer object to store vertex buffer
-            this.vertexBuffer = gl.createBuffer();
-
-            // Bind appropriate array buffer to it
-            gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-
-            // Pass the vertex data to the buffer
-            gl.bufferData(gl.ARRAY_BUFFER, this.vertexBuffer, gl.STATIC_DRAW);
-
-            // Unbind the buffer
-            gl.bindBuffer(gl.ARRAY_BUFFER, null);
-        }
-
-        if(!this.indexBuffer)
-        {
-
-            // Create an empty buffer object to store Index buffer
-            this.indexBuffer = gl.createBuffer();
-
-            // Bind appropriate array buffer to it
-            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-
-            // Pass the vertex data to the buffer
-            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.index, gl.STATIC_DRAW);
-
-            // Unbind the buffer
-            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-        }
+    public bind()
+    {
+        this.vertexBuffer.bind();
+        this.indexBuffer.bind();
     }
 
-    bindBuffer(gl:WebGLRenderingContext){
+    public unbind(){
 
-        // Bind appropriate array buffer to it
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-
-        // Bind appropriate array buffer to it
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-
-    }
-
-
-    unbindBuffer(gl:WebGLRenderingContext){
-
-        // Bind appropriate array buffer to it
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-        // Bind appropriate array buffer to it
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
+        this.vertexBuffer.unbind();
+        this.indexBuffer.unbind();
     }
 }
